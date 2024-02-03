2126f72ba3fe78367aef3e35550293ee52b56591
Dario <darionuberu@gmail.com>
2024-02-03 20:17:37 +0100

    feat: non null checks in LoginDto

--- api/src/main/java/lab/en2b/quizapi/auth/dtos/LoginDto.java
+++ api/src/main/java/lab/en2b/quizapi/auth/dtos/LoginDto.java
@@ -3,11 +3,14 @@ package lab.en2b.quizapi.auth.dtos;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class LoginDto {
+    @NonNull
     private String email;
+    @NonNull
     private String password;
 }
