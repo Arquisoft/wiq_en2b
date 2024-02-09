1b1a91f450453ba27de1b0c79b0ee359d8e2593f
jjgancfer <jjgancfer@gmail.com>
2024-02-09 17:25:45 +0100

    chore: installed chakra ui and removed mui

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import router from 'components/Router';
+import { ChakraProvider } from '@chakra-ui/react';
 
 const root = ReactDOM.createRoot(document.querySelector("body"));
 const browserRouter = createBrowserRouter(router);
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={browserRouter} />
-  </React.StrictMode>
+  <ChakraProvider>
+    <React.StrictMode>
+      <RouterProvider router={browserRouter} />
+    </React.StrictMode>
+  </ChakraProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
