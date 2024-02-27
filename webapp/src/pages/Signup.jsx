1a032b8ef27949b6404d2a4b83bbaf588dd40865
Sergio Rodriguez <uo282598@uniovi.es>
2024-02-27 23:05:49 +0100

    feat: Changing the UI of the Signup

--- webapp/src/pages/Signup.jsx
+++ webapp/src/pages/Signup.jsx
@@ -1,15 +1,22 @@
 import { Center } from "@chakra-ui/layout";
-import { Button, FormControl, FormLabel, Heading, Input, Text, Stack } from "@chakra-ui/react";
+import { Heading, Input, Button, InputGroup, Stack, InputLeftElement, chakra, Box, Avatar, FormControl, InputRightElement, Text } from "@chakra-ui/react";
 import axios, { HttpStatusCode } from "axios";
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
+import { FaUserAlt, FaLock } from "react-icons/fa";
 
 export default function Signup() {
 
     const [hasError, setHasError] = useState(false);
     const navigate = useNavigate();
     const { t } = useTranslation();
+
+    const [showPassword, setShowPassword] = useState(false);
+    const changeShowP = () => setShowPassword(!showPassword);
+
+    const ChakraFaUserAlt = chakra(FaUserAlt);
+    const ChakraFaLock = chakra(FaLock);
 
     const sendLogin = async () => {
         let data = {};
@@ -22,27 +29,53 @@ export default function Signup() {
     }
 
     return (
-        <Center display={"flex"} flexDirection={"column"} maxW={"100%"} minW={"30%"} mt={"2vh"}>
-            <Heading as="h2">{ t("common.register")}</Heading>
+        <Center display={"flex"} flexDirection={"column"} w={"100wh"} h={"100vh"}
+            bg={"blue.50"} justifyContent={"center"} alignItems={"center"}>
             {hasError && (
                 <div className="error-container">
                     <Text>Error</Text>
                 </div>
             )}
-            <Stack spacing={4} mt={4} width="100%" mx={"auto"} maxWidth={"400px"}>
-                <FormControl as="fieldset" padding={"1vh 0vw"} isRequired>
-                    <FormLabel>{ t("session.username") }</FormLabel>
-                    <Input type="text" />
-                </FormControl>
-                <FormControl as="fieldset" padding={"1vh 0vw"} isRequired>
-                    <FormLabel>{ t("Correo electrónico") }</FormLabel> {/* To be changed */}  
-                    <Input type="text" />
-                </FormControl>
-                <FormControl as="fieldset" padding={"1vh 0vw"} isRequired>
-                    <FormLabel> {t("session.password")}</FormLabel>
-                    <Input type="password" />
-                </FormControl>
-                <Button type="submit" colorScheme={"blue"} onClick={sendLogin}>Enviar</Button>
+            <Stack flexDir={"column"} mb="2" justifyContent="center" alignItems={"center"}>
+                <Avatar bg="blue.500" />
+                <Heading as="h2" color="blue.400">{ t("common.register")}</Heading>
+                { 
+                    !hasError ? 
+                    <></> : 
+                    <Center bgColor={"#FFA98A"} margin={"1vh 0vw"} padding={"1vh 0vw"} 
+                        color={"#FF0500"} border={"0.1875em solid #FF0500"}
+                        borderRadius={"0.75em"} maxW={"100%"} minW={"30%"}>
+                            <Text>Error</Text>
+                    </Center> 
+                }
+                <Box minW={{md: "400px"}}>
+                    <Stack spacing={4} p="1rem" backgroundColor="whiteAlpha.900" boxShadow="md">
+                        <FormControl>
+                            <InputGroup>
+                                <InputLeftElement children={<ChakraFaUserAlt color="gray.300" />}/>
+                                <Input type="text" placeholder={t("Correo Electrónico")} />
+                            </InputGroup>
+                        </FormControl>
+                        <FormControl>
+                            <InputGroup>
+                                <InputLeftElement children={<ChakraFaUserAlt color="gray.300" />}/>
+                                <Input type="text" placeholder={t("session.username")} />
+                            </InputGroup>
+                        </FormControl>
+                        <FormControl>
+                            <InputGroup>
+                                <InputLeftElement children={<ChakraFaLock color="gray.300" />}/>
+                                <Input type={showPassword ? "text" : "password"}  placeholder={t("session.password")}/>
+                                <InputRightElement width="4.5rem">
+                                    <Button h="1.75rem" size="sm" onClick={changeShowP}>{
+                                        showPassword ? "Hide" : "Show"
+                                    }</Button>
+                                </InputRightElement>
+                            </InputGroup>
+                        </FormControl>
+                        <Button type="submit" variant="solid" colorScheme="blue" onClick={sendLogin}>Sign Up</Button>
+                    </Stack>
+                </Box>
             </Stack>
         </Center>
     );
