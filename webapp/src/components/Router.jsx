ea3dd1c7e5ce77a4b3ec1fb4ec389264731147f5
sergiorodriguezgarcia <113514397+sergiorodriguezgarcia@users.noreply.github.com>
2024-02-27 18:03:24 +0100

    feat: Creating the signup view

--- webapp/src/components/Router.jsx
+++ webapp/src/components/Router.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Root from "../pages/Root";
 import Login from "../pages/Login";
-// import Register from "../pages/Register";
+import Signup from "../pages/Signup";
 import Layout from "./Layout";
 
 const router = [
@@ -15,11 +15,11 @@ const router = [
       },{
       path: "/login",
       element: <Login />
+      },
+      {
+        path: "/signup",
+        element: <Signup />
       }
-      // {
-      // path: "/register",
-      // element: <Register />
-      // }
     ]
   }
 ];
