3707f2b33a22522d56713d91aa4368815cc72dc3
Gonzalo Alonso Fernandez <gony.soft@outlook.com>
2024-02-27 20:37:34 +0100

    feat: changing the complete look and feel of the login. Deleting the css.

--- webapp/src/components/Router.jsx
+++ webapp/src/components/Router.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Root from "../pages/Root";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
-import Layout from "./Layout";
+// import Layout from "./Layout";
 
 const router = [
   {
