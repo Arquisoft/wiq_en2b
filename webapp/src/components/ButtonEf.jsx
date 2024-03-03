348eb38b441676a22a1573173d609701b0562888
Gonzalo Alonso Fernandez <gony.soft@outlook.com>
2024-03-03 14:41:36 +0100

    feat: trying to fix sonarCloud errors.

--- webapp/src/components/ButtonEf.jsx
+++ webapp/src/components/ButtonEf.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button } from "@chakra-ui/react";
+import PropTypes from 'prop-types';
 import '../styles/AppView.css';
 
 const ButtonEf = ({ dataTestId, variant, colorScheme, text, onClick }) => {
@@ -7,4 +8,13 @@ const ButtonEf = ({ dataTestId, variant, colorScheme, text, onClick }) => {
       <Button type="submit" data-testid={dataTestId} variant={variant} colorScheme={colorScheme} margin={"10px"} className={"custom-button effect1"} onClick={onClick}>{text}</Button>
     );
 };
+
+ButtonEf.propTypes = {
+  dataTestId: PropTypes.string.isRequired,
+  variant: PropTypes.string.isRequired,
+  colorScheme: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
 export default ButtonEf;
