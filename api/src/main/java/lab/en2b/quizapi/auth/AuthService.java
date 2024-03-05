15b6406fdac5d244d73fc952b164d642fef675aa
Dario <darionuberu@gmail.com>
2024-03-05 18:36:03 +0100

    chore: unused import

--- api/src/main/java/lab/en2b/quizapi/auth/AuthService.java
+++ api/src/main/java/lab/en2b/quizapi/auth/AuthService.java
@@ -16,8 +16,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Set;
-
 @Service
 @RequiredArgsConstructor
 public class AuthService {
