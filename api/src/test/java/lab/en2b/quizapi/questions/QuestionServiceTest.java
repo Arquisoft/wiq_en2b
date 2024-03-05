950b3a1b6815ece42600856192acf65aca8b027e
sergioqfeg1 <sergioqfeg@gmail.com>
2024-03-05 18:05:06 +0100

    test: question by id

--- api/src/test/java/lab/en2b/quizapi/questions/QuestionServiceTest.java
+++ api/src/test/java/lab/en2b/quizapi/questions/QuestionServiceTest.java
@@ -10,11 +10,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith({MockitoExtension.class, SpringExtension.class})
@@ -26,6 +31,7 @@ public class QuestionServiceTest {
     QuestionRepository questionRepository;
 
     Question defaultQuestion;
+    QuestionResponseDto defaultResponseDto;
 
     @BeforeEach
     void setUp() {
@@ -40,15 +46,7 @@ public class QuestionServiceTest {
                 .answerCategory(AnswerCategory.CITY)
                 .type(QuestionType.TEXT)
                 .build();
-
-    }
-
-    @Test
-    void testGetRandomQuestion() {
-        when(questionRepository.findRandomQuestion()).thenReturn(defaultQuestion);
-        QuestionResponseDto response =  questionService.getRandomQuestion();
-
-        assertEquals(response, QuestionResponseDto.builder()
+        defaultResponseDto = QuestionResponseDto.builder()
                 .id(1L)
                 .content("What is the capital of France?")
                 .answers(new ArrayList<>())
@@ -56,7 +54,29 @@ public class QuestionServiceTest {
                 .questionCategory(QuestionCategory.GEOGRAPHY)
                 .answerCategory(AnswerCategory.CITY)
                 .type(QuestionType.TEXT)
-                .build());
+                .build();
+    }
+
+    @Test
+    void testGetRandomQuestion() {
+        when(questionRepository.findRandomQuestion()).thenReturn(defaultQuestion);
+        QuestionResponseDto response =  questionService.getRandomQuestion();
+
+        assertEquals(response, defaultResponseDto);
+    }
+
+    @Test
+    void testGetQuestionById(){
+        when(questionRepository.findById(any())).thenReturn(Optional.of(defaultQuestion));
+        QuestionResponseDto response = questionService.getQuestionById(1L);
+
+        assertEquals(response, defaultResponseDto);
+    }
+
+    @Test
+    void testGetQuestionByIdNotFound(){
+        when(questionRepository.findById(any())).thenReturn(Optional.empty());
+        assertThrows(NoSuchElementException.class,() -> questionService.getQuestionById(1L));
     }
 
 }
