4b193d30c42e2b369d816ca637b55979e783c9b4
jjgancfer <jjgancfer@gmail.com>
2024-03-05 12:46:32 +0100

    chore: rewrite to make use of async/await

--- webapp/src/components/auth/AuthUtils.js
+++ webapp/src/components/auth/AuthUtils.js
@@ -1,7 +1,7 @@
-import axios from "axios";
+import axios, { HttpStatusCode } from "axios";
 
 export function isUserLogged() {
-    return axios.defaults.headers.common["Authorization"] != null;
+    return localStorage.getItem("jwtToken") != undefined;
 }
 
 export function saveToken(requestAnswer) {
@@ -10,7 +10,13 @@ export function saveToken(requestAnswer) {
     localStorage.setItem("jwtRefreshToken", requestAnswer.data.refresh_Token)
 }
 
-export function login(loginData) {
-    return axios.post(process.env.REACT_APP_API_ENDPOINT
+export async function login(loginData, onSuccess, onError) {
+    let requestAnswer =  await axios.post(process.env.REACT_APP_API_ENDPOINT
         + process.env.REACT_APP_LOGIN_ENDPOINT, loginData);
+    if (HttpStatusCode.Ok === requestAnswer.status) {
+        saveToken(requestAnswer);
+        onSuccess();
+    } else {
+        onError();
+    }
 }
