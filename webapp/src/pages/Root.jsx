4c10145886c47a7aa03f8727c64d1fedccd105c3
sergiorodriguezgarcia <113514397+sergiorodriguezgarcia@users.noreply.github.com>
2024-03-05 14:10:43 +0100

    feat: Adding the home page and tests for it

--- webapp/src/pages/Root.jsx
+++ webapp/src/pages/Root.jsx
@@ -1,5 +1,23 @@
+import { Center } from "@chakra-ui/layout";
+import { Heading, Stack } from "@chakra-ui/react";
 import React from "react";
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
+import ButtonEf from '../components/ButtonEf';
 
 export default function Root() {
-    return <p>Proof of concept</p>
+    const navigate = useNavigate();
+    const { t } = useTranslation();
+
+    return (
+        <Center display={"flex"} flexDirection={"column"} w={"100wh"} h={"100vh"}
+            bg={"blue.50"} justifyContent={"center"} alignItems={"center"}>
+            <Heading as="h1" color="blue.400">{"WIQ-EN2B"}</Heading>
+            <p>Welcome to the WIQ-EN2B page</p>
+            <Stack spacing={4} p="3rem">
+                <ButtonEf dataTestId={"Login"} variant={"solid"} colorScheme={"blue"} text={t("common.login")} onClick={() => navigate("/login")}/>
+                <p onClick={() => navigate("/signup")} style={{ cursor: 'pointer' }}>You don´t have an account?</p>
+            </Stack>  
+        </Center>
+    );
 }
