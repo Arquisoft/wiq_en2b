74c4e8642f2c6cea86395c336154bb6ebb805231
Gonzalo Alonso Fernandez <gony.soft@outlook.com>
2024-03-08 19:49:54 +0100

    fix: fixing errors with the environment.

--- api/src/main/java/lab/en2b/quizapi/questions/question/QuestionRepository.java
+++ api/src/main/java/lab/en2b/quizapi/questions/question/QuestionRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface QuestionRepository extends JpaRepository<Question,Long> {
-    @Query(value = "SELECT * FROM questions ORDER BY RAND() LIMIT 1", nativeQuery = true)
+    @Query(value = "SELECT * FROM questions ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
     Question findRandomQuestion();
 }
--- webapp/src/components/game/Questions.js
+++ webapp/src/components/game/Questions.js
@@ -1,14 +1,12 @@
 import axios, { HttpStatusCode } from "axios";
 
-export async function getQuestion(onError) {
+export async function getQuestion() {
     try {
         let requestAnswer = await axios.get(process.env.REACT_APP_API_ENDPOINT + "/questions/new");
         if (HttpStatusCode.Ok === requestAnswer.status) {
             return requestAnswer.data;
-        } else {
-            onError();
         }
     } catch {
-        onError();
+        
     }
 }
