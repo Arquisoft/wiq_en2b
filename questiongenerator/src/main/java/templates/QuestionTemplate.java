5c9aaccab15af2bb37aca595ef9c40bff4c0320e
Gonzalo Suárez Losada <uo283928>
2024-03-08 19:37:58 +0100

    chore: Comments added on QuestionTemplate and small code clean ups.

--- questiongenerator/src/main/java/templates/QuestionTemplate.java
+++ questiongenerator/src/main/java/templates/QuestionTemplate.java
@@ -9,14 +9,27 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+
+/**
+ * Base template for questions to be implemented in the future.
+ */
 public abstract class QuestionTemplate {
+    // Query to be sent to WikiData QS
     protected String sparqlQuery;
+    // Response given by WikiData QS for the query sent
     protected JSONArray results;
+    // Language code representing in what language the query must be sent. Spanish as a default value.
     protected String langCode = "es";
 
     protected AnswerRepositoryImpl answerRepository;
     protected QuestionRepositoryImpl questionRepository;
 
+    /**
+     * Constructor for QuestionTemplates which is also the one in charge of the whole question retrieval process for a query
+     * For future types of question, only need to override abstract methods and calling super() on constructor
+     * When instancing a question, only constructor invocation is required.
+     * @param langCode
+     */
     public QuestionTemplate(String langCode) {
         this.answerRepository = new AnswerRepositoryImpl();
         this.questionRepository = new QuestionRepositoryImpl();
@@ -27,9 +40,23 @@ public abstract class QuestionTemplate {
         processResults();
     }
 
-    public abstract void setQuery();
+    /**
+     * Update the value of @sparqlQuery with the query to be sent.
+     */
+    protected abstract void setQuery();
 
-    public void call() {
+    /**
+     * Method for the whole processing the @results given by WikiData QS as a JSON.
+     * It also is in charge of storing both the processed answers and the question in all languages
+     */
+    protected abstract void processResults();
+
+    /**
+     * Method in charge of the HTTP request with WikiData QS.
+     * It allows to send only one query, so it does not support questions whose answer require multiple queries.
+     * CAUTION: Remember to update the results field of the field if this method gets overwritten.
+     */
+    private void call() {
         // Set up the HTTP client
         HttpClient httpClient = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
@@ -43,17 +70,14 @@ public abstract class QuestionTemplate {
         HttpResponse<String> response = null;
         try {
             response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (InterruptedException e) {
+        } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
+
         JSONObject jsonResponse = new JSONObject(response.body());
         JSONArray results = jsonResponse.getJSONObject("results").getJSONArray("bindings");
 
         this.results = results; // Save the results. If this method is overwritten this line MUST be kept
     }
-
-    public abstract void processResults();
 
 }
