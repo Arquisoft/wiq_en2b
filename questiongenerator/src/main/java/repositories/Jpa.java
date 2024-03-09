4a2ae9cc7e4ad7bfa3dd447e41a143ee14df2c79
Gonzalo Suárez Losada <uo283928>
2024-03-09 14:06:42 +0100

    chore: Added documentation to Jpa

--- questiongenerator/src/main/java/repositories/Jpa.java
+++ questiongenerator/src/main/java/repositories/Jpa.java
@@ -5,10 +5,19 @@ import javax.persistence.Persistence;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Class in charge of setting the connection to the DB
+ * It is an implementation of Singleton DP until the connection gets shut down though close() call.
+ * As it is not possible to reopen connections, a new one must be created if access to DB is desired.
+ */
 public class Jpa {
 
+    // Singleton
     private static EntityManagerFactory emf = null;
 
+    /**
+     * Sets the enviroment variables for the DB connection.
+     */
     public static EntityManagerFactory getEntityManagerFactory() {
         if (emf == null) {
             Map<String, String> properties = new HashMap<>();
@@ -26,6 +35,9 @@ public class Jpa {
         return emf;
     }
 
+    /**
+     * Closes the current connection in a null-preventive way.
+     */
     public static void close() {
         if (emf != null) {
             emf.close();
