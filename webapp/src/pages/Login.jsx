a1c75de056f44a847afdbe69522b03a39370d9ca
sergiorodriguezgarcia <113514397+sergiorodriguezgarcia@users.noreply.github.com>
2024-03-09 13:26:02 +0100

    feat: Adding the custom message to the Login view

--- webapp/src/pages/Login.jsx
+++ webapp/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 import { FaLock, FaAddressCard } from "react-icons/fa";
@@ -8,7 +8,7 @@ import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons'
 
 import ButtonEf from '../components/ButtonEf';
 import '../styles/AppView.css';
-import {isUserLogged, login} from "../components/auth/AuthUtils";
+import { isUserLogged, login } from "../components/auth/AuthUtils";
 
 export default function Login() {
 
@@ -29,34 +29,39 @@ export default function Login() {
     const ChakraFaCardAlt = chakra(FaAddressCard);
     const ChakraFaLock = chakra(FaLock);
 
-    const sendLogin = async () => {
+    const sendLogin = async (errorMessage) => {
         const loginData = {
             "email": document.getElementById("user").value,
             "password": document.getElementById("password").value
         };
         await login(loginData, navigateToDashboard, () => setHasError(true));
+        if (errorMessage) {
+            setErrorMessage(errorMessage);
+        }
     }
+
+    const [errorMessage, setErrorMessage] = useState("");
 
     return (
         <Center display={"flex"} flexDirection={"column"} w={"100wh"} h={"100vh"}
             bg={"blue.50"} justifyContent={"center"} alignItems={"center"}>
             <Stack flexDir={"column"} mb="2" justifyContent="center" alignItems={"center"}>
                 <Avatar bg="blue.500" />
-                <Heading as="h2" color="blue.400">{ t("common.login")}</Heading>
-                { 
-                    hasError && 
+                <Heading as="h2" color="blue.400">{t("common.login")}</Heading>
+                {
+                    hasError &&
                     <Alert status='error' rounded="1rem" margin={"1vh 0vw"}>
                         <AlertIcon />
                         <AlertTitle>{t("error.login")}</AlertTitle>
-                        <AlertDescription>{t("error.login-desc")}</AlertDescription>
+                        <AlertDescription>{errorMessage ? errorMessage : t("error.login-desc")}</AlertDescription>
                     </Alert>
                 }
-                <Box minW={{md: "400px"}} shadow="2xl">
+                <Box minW={{ md: "400px" }} shadow="2xl">
                     <Stack spacing={4} p="1rem" backgroundColor="whiteAlpha.900" boxShadow="md" rounded="1rem">
                         <FormControl>
                             <InputGroup>
                                 <InputLeftElement>
-                                    <ChakraFaCardAlt color="gray.300"/>
+                                    <ChakraFaCardAlt color="gray.300" />
                                 </InputLeftElement>
                                 <Input type="text" id={"user"} placeholder={t("session.email")} />
                             </InputGroup>
@@ -66,13 +71,13 @@ export default function Login() {
                                 <InputLeftElement>
                                     <ChakraFaLock color="gray.300" />
                                 </InputLeftElement>
-                                <Input type={showPassword ? "text" : "password"} id={"password"} placeholder={t("session.password")}/>
+                                <Input type={showPassword ? "text" : "password"} id={"password"} placeholder={t("session.password")} />
                                 <InputRightElement>
-                                    <IconButton h="1.75rem" size="sm" onClick={changeShowP} aria-label='Shows or hides the password' icon={showPassword ? <ViewOffIcon/> : <ViewIcon/>} data-testid="togglePasswordButton"/>
+                                    <IconButton h="1.75rem" size="sm" onClick={changeShowP} aria-label='Shows or hides the password' icon={showPassword ? <ViewOffIcon /> : <ViewIcon />} data-testid="togglePasswordButton" />
                                 </InputRightElement>
                             </InputGroup>
                         </FormControl>
-                        <ButtonEf dataTestId={"Login"} variant={"solid"} colorScheme={"blue"} text={t("common.login")} onClick={sendLogin}/>
+                        <ButtonEf dataTestId={"Login"} variant={"solid"} colorScheme={"blue"} text={t("common.login")} onClick={() => sendLogin(t("error.login-send"))} />
                     </Stack>
                 </Box>
             </Stack>
