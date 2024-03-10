3687a932cc661c11888c66789321a70906ed11b4
Dario <darionuberu@gmail.com>
2024-03-10 10:06:27 +0100

    fix: logout not removing session storage

--- webapp/src/components/game/Logout.js
+++ webapp/src/components/game/Logout.js
@@ -3,6 +3,8 @@ import axios from "axios";
 export async function logoutUser() {
     try {
         await axios.get(process.env.REACT_APP_API_ENDPOINT + "/auth/logout");
+        sessionStorage.removeItem("jwtToken");
+        sessionStorage.removeItem("jwtRefreshToken");
     } catch (error) {
         console.error("Error logging out user: ", error);
     }
