26b8a7adbe75238a1ffb04daeef7c277c7260465
Gonzalo Alonso Fernandez <gony.soft@outlook.com>
2024-03-10 10:43:11 +0100

    feat: adding more tests cases for the dashboard.

--- webapp/src/tests/Dashboard.test.js
+++ webapp/src/tests/Dashboard.test.js
@@ -2,44 +2,60 @@ import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router';
 import Dashboard from '../pages/Dashboard';
+import ButtonEf from '../components/ButtonEf';
 
 describe('Dashboard component', () => {
   it('renders dashboard elements correctly', async () => {
     const { getByText } = render(<MemoryRouter><Dashboard/></MemoryRouter>);
 
-    // Check if the heading is rendered
     expect(getByText("common.dashboard")).toBeInTheDocument();
 
-    // Check if the buttons are rendered
     expect(screen.getByTestId('Rules')).toBeInTheDocument();
     expect(screen.getByTestId('Play')).toBeInTheDocument();
     expect(screen.getByTestId('Statistics')).toBeInTheDocument();
 
-    // Check if the logout button is rendered
     expect(screen.getByText(/logout/i)).toBeInTheDocument();
   });
 
   it('navigates to the rules route on button click', () => {
-    // Render the component
     render(<MemoryRouter><Dashboard /></MemoryRouter>);
   
     const rulesButton = screen.getByTestId('Rules');
     fireEvent.click(rulesButton);
   
-    // Check the change of path
     expect(screen.getByText("common.rules")).toBeInTheDocument();
   });
 
   it('do not navigates to the statistics route on button click', () => {
-    // Render the component
     render(<MemoryRouter><Dashboard /></MemoryRouter>);
   
     const statisticsButton = screen.getByTestId('Statistics');
     fireEvent.click(statisticsButton);
   
-    // Check the change of path
     expect(screen.getByText("common.dashboard")).toBeInTheDocument();
   });
 
-  // Test the play and log out buttons.
+  it('navigates to the game route on "Play" button click', () => {
+    render(<MemoryRouter><Dashboard /></MemoryRouter>);
+  
+    const playButton = screen.getByTestId('Play');
+    fireEvent.click(playButton);
+  
+    expect(screen.getByText("common.play")).toBeInTheDocument();
+  });
+
+  it('does not navigate to the statistics route on button click', () => {
+    render(<MemoryRouter><Dashboard /></MemoryRouter>);
+  
+    const statisticsButton = screen.getByTestId('Statistics');
+    fireEvent.click(statisticsButton);
+  
+    expect(screen.getByText("common.dashboard")).toBeInTheDocument();
+  });
+
+  it('renders ButtonEf correctly', () => {
+    const { getByTestId } = render(<ButtonEf dataTestId="TestId" variant="outline" colorScheme="blue" text="Test Text" onClick={() => {}} />);
+  
+    expect(getByTestId('TestId')).toBeInTheDocument();
+  });
 });
