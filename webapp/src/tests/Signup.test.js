57f908c2cf6ab51a1b7b919bb1d46d338b91e81a
Gonzalo Alonso Fernandez <gony.soft@outlook.com>
2024-03-10 11:26:13 +0100

    feat: adding more tests to the signup.

--- webapp/src/tests/Signup.test.js
+++ webapp/src/tests/Signup.test.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { render, fireEvent, waitFor, getByTestId, getAllByTestId } from '@testing-library/react';
-import axios from 'axios';
+import { render, fireEvent, getByTestId, getAllByTestId } from '@testing-library/react';
 import { MemoryRouter } from 'react-router';
 import Signup from '../pages/Signup';
-import { register as mockRegister } from '../components/auth/AuthUtils';
 
 jest.mock('../components/auth/AuthUtils', () => ({
   isUserLogged: jest.fn(),
@@ -35,16 +33,31 @@ describe('Signup Component', () => {
   it('submits form data correctly', async () => {
     const { getByPlaceholderText, getByTestId } = render(<MemoryRouter><Signup /></MemoryRouter>);
   
-    // Get form elements and submit button by their text and placeholder values
     const emailInput = getByPlaceholderText('session.email');
     const usernameInput = getByPlaceholderText('session.username');
     const passwordInput = getByPlaceholderText('session.password');
     const signUpButton = getByTestId('Sign up');
-    
-    // Fill out the form with valid data and submit it
+
     fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
     fireEvent.change(usernameInput, { target: { value: 'testuser' } });
     fireEvent.change(passwordInput, { target: { value: 'password' } });
     fireEvent.click(signUpButton);
+  });
+  it('toggles confirm password visibility', () => {
+    const { getAllByTestId, getByPlaceholderText } = render(<MemoryRouter><Signup /></MemoryRouter>);
+    getByPlaceholderText('session.confirm_password');
+    const toggleButton = getAllByTestId('show-confirm-password-button')[1];
+  
+    fireEvent.click(toggleButton);
+
+    const confirmPasswordInput = getByPlaceholderText('session.confirm_password');
+    expect(confirmPasswordInput.getAttribute('type')).toBe('text');
+  });
+  it('handles confirm password change', () => {
+    const { getByPlaceholderText } = render(<MemoryRouter><Signup /></MemoryRouter>);
+    const confirmPasswordInput = getByPlaceholderText('session.confirm_password');
+  
+    fireEvent.change(confirmPasswordInput, { target: { value: 'newPassword' } });
+    expect(confirmPasswordInput.value).toBe('newPassword');
   });
 });
