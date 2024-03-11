da1c2671ed8398b484f0e314f009aaa2668521f1
sergiorodriguezgarcia <113514397+sergiorodriguezgarcia@users.noreply.github.com>
2024-03-11 14:32:42 +0100

    feat: Adding tests to the Results.jsx

---
+++ webapp/src/tests/Results.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { render, fireEvent, waitFor, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { BrowserRouter, MemoryRouter } from 'react-router-dom';
+import Results from '../pages/Results';
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useLocation: () => ({
+    state: { correctAnswers: 3 },
+  }),
+}));
+
+describe('Results Component', () => {
+    test('renders results with correct answers', () => {
+        const { getByText, getByTestId } = render(
+        <BrowserRouter>
+            <Results />
+        </BrowserRouter>
+        );
+
+        expect(getByText('Results')).toBeInTheDocument();
+        expect(getByText('Correct answers: 3')).toBeInTheDocument();
+        expect(getByTestId('GoBack')).toBeInTheDocument();
+        expect(getByTestId('GoBack')).toHaveTextContent('common.finish');
+    });
+
+    it('navigates to dashboard on button click', async () => {
+        const navigateMock = jest.fn();
+        jest.spyOn(require('react-router-dom'), 'useNavigate').mockReturnValue(navigateMock);
+
+        const { getByTestId } = render(
+        <BrowserRouter>
+            <Results />
+        </BrowserRouter>
+        );
+
+        const goBackButton = getByTestId('GoBack');
+        fireEvent.click(goBackButton);
+
+        await waitFor(() => {
+        expect(navigateMock).toHaveBeenCalled();
+        expect(navigateMock).toHaveBeenCalledWith('/dashboard');
+        });
+    });
+});
