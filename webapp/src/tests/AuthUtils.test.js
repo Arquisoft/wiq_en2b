a0e8a81248834ac63a047509c79ded82756c948f
Gonzalo Alonso Fernandez <gony.soft@outlook.com>
2024-03-25 19:59:21 +0100

    feat: adding tests for the register part in authutils

--- webapp/src/tests/AuthUtils.test.js
+++ webapp/src/tests/AuthUtils.test.js
@@ -1,6 +1,6 @@
 import MockAdapter from "axios-mock-adapter";
 import axios, { HttpStatusCode } from "axios";
-import {isUserLogged, login, saveToken} from "components/auth/AuthUtils";
+import {isUserLogged, login, register, saveToken} from "components/auth/AuthUtils";
 
 const mockAxios = new MockAdapter(axios);
 
@@ -71,6 +71,23 @@ describe("Auth Utils tests", () => {
             expect(mockOnError).toHaveBeenCalled();
             expect(isUserLogged()).toBe(false);
         });
+        it("handles login error with other status code", async () => {
+            mockAxios.onPost().replyOnce(HttpStatusCode.InternalServerError);
+
+            const mockOnSuccess = jest.fn();
+            const mockOnError = jest.fn();
+
+            const loginData = {
+                "email": "test@email.com",
+                "password": "test"
+            };
+
+            await login(loginData, mockOnSuccess, mockOnError, jest.fn());
+
+            expect(mockOnSuccess).not.toHaveBeenCalled();
+            expect(mockOnError).toHaveBeenCalled();
+            expect(isUserLogged()).toBe(false);
+        });
     });
 
     describe("when the user is authenticated", () => {
@@ -108,6 +125,92 @@ describe("Auth Utils tests", () => {
             saveToken(mockResponse);
             expect(sessionStorage.getItem("jwtToken")).toBe(mockResponse.data.token);
             expect(sessionStorage.getItem("jwtRefreshToken")).toBe(mockResponse.data.refresh_token);
+        });
+    });
+
+    describe("register", () => {
+        beforeEach(() => {
+            sessionStorage.clear();
+            mockAxios.reset();
+        });
+
+        it("handles successful registration", async () => {
+            const registerData = {
+                "email": "test@email.com",
+                "password": "test"
+            };
+
+            const onSuccess = jest.fn();
+            const onError = jest.fn();
+
+            mockAxios.onPost().replyOnce(HttpStatusCode.Ok, {
+                "token": "token",
+                "refresh_token": "refreshToken"
+            });
+
+            await register(registerData, onSuccess, onError, jest.fn());
+
+            expect(onSuccess).toHaveBeenCalled();
+            expect(onError).not.toHaveBeenCalled();
+            expect(sessionStorage.getItem("jwtToken")).toBe("token");
+            expect(sessionStorage.getItem("jwtRefreshToken")).toBe("refreshToken");
+        });
+
+        it("handles registration error with status code 400", async () => {
+            const registerData = {
+                "email": "test@email.com",
+                "password": "test"
+            };
+
+            const onSuccess = jest.fn();
+            const onError = jest.fn();
+
+            mockAxios.onPost().replyOnce(HttpStatusCode.BadRequest);
+
+            await register(registerData, onSuccess, onError, jest.fn());
+
+            expect(onSuccess).not.toHaveBeenCalled();
+            expect(onError).toHaveBeenCalled();
+            expect(sessionStorage.getItem("jwtToken")).toBe(null);
+            expect(sessionStorage.getItem("jwtRefreshToken")).toBe(null);
+        });
+
+        it("handles registration error with status code 409", async () => {
+            const registerData = {
+                "email": "test@email.com",
+                "password": "test"
+            };
+
+            const onSuccess = jest.fn();
+            const onError = jest.fn();
+
+            mockAxios.onPost().replyOnce(HttpStatusCode.Conflict);
+
+            await register(registerData, onSuccess, onError, jest.fn());
+
+            expect(onSuccess).not.toHaveBeenCalled();
+            expect(onError).toHaveBeenCalled();
+            expect(sessionStorage.getItem("jwtToken")).toBe(null);
+            expect(sessionStorage.getItem("jwtRefreshToken")).toBe(null);
+        });
+
+        it("handles registration error with unknown status code", async () => {
+            const registerData = {
+                "email": "test@email.com",
+                "password": "test"
+            };
+
+            const onSuccess = jest.fn();
+            const onError = jest.fn();
+
+            mockAxios.onPost().replyOnce(HttpStatusCode.InternalServerError);
+
+            await register(registerData, onSuccess, onError, jest.fn());
+
+            expect(onSuccess).not.toHaveBeenCalled();
+            expect(onError).toHaveBeenCalled();
+            expect(sessionStorage.getItem("jwtToken")).toBe(null);
+            expect(sessionStorage.getItem("jwtRefreshToken")).toBe(null);
         });
     });
 });
