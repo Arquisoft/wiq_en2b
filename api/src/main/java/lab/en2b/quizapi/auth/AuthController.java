32b08ac8cbfaa020d06defe702f7ddfb1c8fe305
Dario <darionuberu@gmail.com>
2024-03-27 18:50:52 +0100

    fix: descriptions for errors no longer contain response body

--- api/src/main/java/lab/en2b/quizapi/auth/AuthController.java
+++ api/src/main/java/lab/en2b/quizapi/auth/AuthController.java
@@ -19,8 +19,8 @@ public class AuthController {
     @Operation(summary = "Registers an user", description = "Registers an user and returns the login dto if it was 200")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Successfully retrieved, returns the login dto"),
-            @ApiResponse(responseCode = "400", description = "Invalid email format or credentials (username or email) already in use"),
-            @ApiResponse(responseCode = "401", description = "Invalid email or password, check for them to be correct"),
+            @ApiResponse(responseCode = "400", description = "Invalid email format or credentials (username or email) already in use", content = @io.swagger.v3.oas.annotations.media.Content),
+            @ApiResponse(responseCode = "401", description = "Invalid email or password, check for them to be correct", content = @io.swagger.v3.oas.annotations.media.Content),
 
     })
     @PostMapping("/register")
@@ -32,8 +32,8 @@ public class AuthController {
     @Operation(summary = "Logs in an user", description = "Given a login dto, logs in that user")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
-            @ApiResponse(responseCode = "400", description = "Invalid email format"),
-            @ApiResponse(responseCode = "401", description = "Invalid email or password, check for them to be correct"),
+            @ApiResponse(responseCode = "400", description = "Invalid email format", content = @io.swagger.v3.oas.annotations.media.Content),
+            @ApiResponse(responseCode = "401", description = "Invalid email or password, check for them to be correct", content = @io.swagger.v3.oas.annotations.media.Content),
 
     })
     @PostMapping("/login")
@@ -44,7 +44,7 @@ public class AuthController {
     @Operation(summary = "Logs the current user out")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
-            @ApiResponse(responseCode = "403", description = "You are not logged in, so there's no log out to do"),
+            @ApiResponse(responseCode = "403", description = "You are not logged in, so there's no log out to do", content = @io.swagger.v3.oas.annotations.media.Content),
     })
     @GetMapping("/logout")
     public ResponseEntity<Void> logoutUser(Authentication authentication){
@@ -55,7 +55,7 @@ public class AuthController {
     @Operation(summary = "Gets a refresh token dto", description = "Asks for a new token and returns a refresh token dto")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
-            @ApiResponse(responseCode = "403", description = "You are not logged in, so there's no token to refresh"),
+            @ApiResponse(responseCode = "403", description = "You are not logged in, so there's no token to refresh", content = @io.swagger.v3.oas.annotations.media.Content),
     })
     @PostMapping("/refresh-token")
     public ResponseEntity<RefreshTokenResponseDto> refreshToken(@Valid @RequestBody RefreshTokenDto refreshTokenRequest){
