fe2ba3e8e45028a8b40b0201918911b42276add2
sergioqfeg1 <sergioqfeg@gmail.com>
2024-03-28 11:48:53 +0100

    test: answer question controller test

--- api/src/test/java/lab/en2b/quizapi/game/GameControllerTest.java
+++ api/src/test/java/lab/en2b/quizapi/game/GameControllerTest.java
@@ -54,7 +54,7 @@ public class GameControllerTest {
 
     @Test
     void startRoundShouldReturn403() throws Exception{
-        mockMvc.perform(post("/games/-1/startRound")
+        mockMvc.perform(post("/games/1/startRound")
                         .contentType("application/json")
                         .with(csrf()))
                 .andExpect(status().isForbidden());
@@ -71,7 +71,7 @@ public class GameControllerTest {
 
     @Test
     void getCurrentQuestionShouldReturn403() throws Exception{
-        mockMvc.perform(get("/games/-1/question")
+        mockMvc.perform(get("/games/1/question")
                         .contentType("application/json")
                         .with(csrf()))
                 .andExpect(status().isForbidden());
@@ -80,6 +80,23 @@ public class GameControllerTest {
     @Test
     void getCurrentQuestionShouldReturn200() throws Exception{
         mockMvc.perform(get("/games/1/question")
+                        .with(user("test").roles("user"))
+                        .contentType("application/json")
+                        .with(csrf()))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void answerQuestionShouldReturn403() throws Exception{
+        mockMvc.perform(post("/games/1/answer?answerId=1")
+                        .contentType("application/json")
+                        .with(csrf()))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void answerQuestionShouldReturn200() throws Exception{
+        mockMvc.perform(post("/games/1/answer?answerId=1")
                         .with(user("test").roles("user"))
                         .contentType("application/json")
                         .with(csrf()))
