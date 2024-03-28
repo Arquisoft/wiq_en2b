328bdcaf863b99d6e3fab6568a860eb2c588bb1d
Gonzalo Alonso Fernandez <gony.soft@outlook.com>
2024-03-28 11:23:21 +0100

    feat: changing the root look and feel.

--- webapp/src/pages/Root.jsx
+++ webapp/src/pages/Root.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 import { Center } from "@chakra-ui/layout";
-import { Text, Heading, Stack, Link, Image } from "@chakra-ui/react";
+import { Text, Heading, Stack, Link, Image, Box } from "@chakra-ui/react";
 
 import ButtonEf from '../components/ButtonEf';
 
@@ -11,20 +11,18 @@ export default function Root() {
     const { t } = useTranslation();
 
     return (
-        <Center display={"flex"} flexDirection={"column"} w={"100wh"} h={"100vh"} bg={"moonstone.900"} justifyContent={"center"} alignItems={"center"}>
-            <Image
-                borderRadius='full'
-                boxSize='150px'
-                src='/kiwiq-icon.ico'
-                alt='kiwiq icon'
-            />
-            <Heading as="h1" bgGradient='linear(to-l, forest_green.400, pigment_green.600)' bgClip='text'>{"KIWIQ"}</Heading>
-
-            <Text>{t("session.welcome")}</Text>
-            <Stack spacing={4} p="3rem">
-                <ButtonEf dataTestId={"Login"} variant={"solid"} colorScheme={"pigment_green"} text={t("common.login")} onClick={() => navigate("/login")}/>
-                <Text>{t("session.account")} <Link as="span" color={"forest_green.400"} onClick={() => navigate("/signup")}>{t("session.clickHere")}</Link></Text>
-            </Stack>  
+        <Center display={"flex"} flexDirection={"column"} w={"100wh"} h={"100vh"} bgImage={'/background.svg'}>
+            <Stack flexDir={"column"} mb="2" justifyContent="center" alignItems={"center"}>
+                <Image borderRadius='full' boxSize='150px' src='/kiwiq-icon.ico' alt='kiwiq icon'/>
+                <Box minW={{md:"400px"}} textAlign="center">
+                    <Stack spacing={4} p="1rem" backgroundColor="whiteAlpha.900" shadow="2xl" rounded="1rem">
+                        <Heading as="h1" bgGradient='linear(to-l, forest_green.400, pigment_green.600)' bgClip='text'>{"KIWIQ"}</Heading>
+                        <Text>{t("session.welcome")}</Text>
+                        <ButtonEf dataTestId={"Login"} variant={"solid"} colorScheme={"pigment_green"} text={t("common.login")} onClick={() => navigate("/login")}/>
+                        <Text>{t("session.account")} <Link as="span" color={"forest_green.400"} onClick={() => navigate("/signup")}>{t("session.clickHere")}</Link></Text>
+                    </Stack>
+                </Box>
+            </Stack> 
         </Center>
     );
 }
