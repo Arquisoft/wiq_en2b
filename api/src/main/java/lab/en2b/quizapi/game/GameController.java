a22ca5c7721597d405065688fffbf8cd72468068
Gonzalo Suárez Losada <uo283928@uniovi.es>
2024-04-01 18:07:03 +0200

    docs: swagger game documentation

--- api/src/main/java/lab/en2b/quizapi/game/GameController.java
+++ api/src/main/java/lab/en2b/quizapi/game/GameController.java
@@ -1,5 +1,6 @@
 package lab.en2b.quizapi.game;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lab.en2b.quizapi.game.dtos.GameAnswerDto;
 import lab.en2b.quizapi.game.dtos.GameResponseDto;
 import lab.en2b.quizapi.questions.question.dtos.QuestionResponseDto;
@@ -14,11 +15,14 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class GameController {
     private final GameService gameService;
+
+    @Operation(summary = "Starts new game", description = "Requests the API to create a new game for a given authentication (a player)")
     @PostMapping("/new")
     public ResponseEntity<GameResponseDto> newGame(Authentication authentication){
         return ResponseEntity.ok(gameService.newGame(authentication));
     }
 
+    @Operation(summary = "Starts a new round", description = "Starts the round (getting a question and its possible answers and start the timer) for a given authentication (a player)")
     @PostMapping("/{id}/startRound")
     public ResponseEntity<GameResponseDto> startRound(@PathVariable Long id, Authentication authentication){
         return ResponseEntity.ok(gameService.startRound(id, authentication));
@@ -29,16 +33,19 @@ public class GameController {
         return ResponseEntity.ok(gameService.getCurrentQuestion(id, authentication));
     }
 
+    @Operation(summary = "Starts a new round", description = "Starts the round (getting a question and its possible answers and start the timer) for a given authentication (a player)")
     @PostMapping("/{id}/answer")
         public ResponseEntity<GameResponseDto> answerQuestion(@PathVariable Long id, @RequestBody GameAnswerDto dto, Authentication authentication){
         return ResponseEntity.ok(gameService.answerQuestion(id, dto, authentication));
     }
 
+    @Operation(summary = "Changing languages", description = "Changes the language of the game for a given authentication (a player) and a language supported. Changes may are applied on the next round.")
     @PutMapping("/{id}/language")
     public ResponseEntity<GameResponseDto> changeLanguage(@PathVariable Long id, @RequestParam String language, Authentication authentication){
         return ResponseEntity.ok(gameService.changeLanguage(id, language, authentication));
     }
 
+    @Operation(summary = "Get the summary of a game")
     @GetMapping("/{id}/details")
     public ResponseEntity<GameResponseDto> getGameDetails(@PathVariable Long id, Authentication authentication){
         return ResponseEntity.ok(gameService.getGameDetails(id, authentication));
