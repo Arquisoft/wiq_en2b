028d4c3be64ef3f58faeda56694589f007543f4a
Gonzalo Alonso Fernandez <gony.soft@outlook.com>
2024-04-02 19:48:51 +0200

    feat: created a new component called MenuButton.

---
+++ webapp/src/components/MenuButton.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import { FaBars } from "react-icons/fa";
+import { chakra, Box, IconButton } from "@chakra-ui/react";
+
+const MenuButton = ({ onClick }) => {
+    return (
+        <Box position="absolute" top="1rem" right="1rem">
+            <IconButton 
+                bg="pigment_green.600"
+                color="whiteAlpha.900"
+                size="lg"
+                borderRadius="md"
+                aria-label="Open Menu"
+                _hover={{ bg: 'pigment_green.400' }}
+                icon={<chakra.span as={FaBars} fontSize="1.5em" />}
+                onClick={onClick}
+            />
+        </Box>
+    );
+}
+
+MenuButton.propTypes = {
+    onClick: PropTypes.func.isRequired
+};
+
+export default MenuButton;
