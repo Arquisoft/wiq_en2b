6d2c85ae24c9dad7bba31fb57394a803a013809c
Diego Villanueva <UO283615@uniovi.es>
2024-04-03 15:23:16 +0200

    Chore: Updated the categories for the answers

--- api/src/main/java/lab/en2b/quizapi/questions/answer/AnswerCategory.java
+++ api/src/main/java/lab/en2b/quizapi/questions/answer/AnswerCategory.java
@@ -1,6 +1,6 @@
 package lab.en2b.quizapi.questions.answer;
 
 public enum AnswerCategory {
-    OTHER, CAPITAL_CITY, COUNTRY, SONG, STADIUM, DATE, PERSON, EVENT
+    CAPITAL_CITY, COUNTRY, SONG, STADIUM, BALLON_DOR
 }
 
