aa6e0df76db7889182760b4c7c73a021b7fa70eb
sergioqfeg1 <sergioqfeg@gmail.com>
2024-04-04 17:32:02 +0200

    feat: implementation of StatisticsController

---
+++ api/src/main/java/lab/en2b/quizapi/statistics/StatisticsController.java
@@ -0,0 +1,11 @@
+package lab.en2b.quizapi.statistics;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/statistics")
+@RequiredArgsConstructor
+public class StatisticsController {
+}
