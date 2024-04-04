8d8c8064507f635b2c4aac0884456fc85ae2f1f4
sergioqfeg1 <sergioqfeg@gmail.com>
2024-04-04 17:37:07 +0200

    feat: method in service for finding statistics by user

--- api/src/main/java/lab/en2b/quizapi/statistics/StatisticsService.java
+++ api/src/main/java/lab/en2b/quizapi/statistics/StatisticsService.java
@@ -1,9 +1,23 @@
 package lab.en2b.quizapi.statistics;
 
+import lab.en2b.quizapi.commons.user.UserService;
+import lab.en2b.quizapi.statistics.dtos.StatisticsResponseDto;
+import lab.en2b.quizapi.statistics.mappers.StatisticsResponseDtoMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class StatisticsService {
+
+    private final StatisticsRepository statisticsRepository;
+    private final UserService userService;
+    private final StatisticsResponseDtoMapper statisticsResponseDtoMapper;
+
+    public StatisticsResponseDto getStatisticsForUser(Authentication authentication){
+        return statisticsResponseDtoMapper.apply(statisticsRepository.findByUserId(userService.
+                getUserByAuthentication(authentication).getId()).orElseThrow());
+    }
+
 }
