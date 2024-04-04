fa2587cc158b79b26e1448ed69cc484e58305285
sergioqfeg1 <sergioqfeg@gmail.com>
2024-04-04 17:30:51 +0200

    feat: implementation of StatisticsService

---
+++ api/src/main/java/lab/en2b/quizapi/statistics/StatisticsService.java
@@ -0,0 +1,9 @@
+package lab.en2b.quizapi.statistics;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class StatisticsService {
+}
