bb325d342197353b401deb2a325222d64c0c7dbc
sergioqfeg1 <sergioqfeg@gmail.com>
2024-04-04 17:39:30 +0200

    feat: get method for personal statistics

--- api/src/main/java/lab/en2b/quizapi/statistics/StatisticsController.java
+++ api/src/main/java/lab/en2b/quizapi/statistics/StatisticsController.java
@@ -1,6 +1,10 @@
 package lab.en2b.quizapi.statistics;
 
+import lab.en2b.quizapi.statistics.dtos.StatisticsResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/statistics")
 @RequiredArgsConstructor
 public class StatisticsController {
+
+    private final StatisticsService statisticsService;
+
+    @GetMapping("/personal")
+    public ResponseEntity<StatisticsResponseDto> getPersonalStatistics(Authentication authentication){
+        return ResponseEntity.ok(statisticsService.getStatisticsForUser(authentication));
+    }
+
 }
