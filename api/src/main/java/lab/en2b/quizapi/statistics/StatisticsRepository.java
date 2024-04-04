8a640f62c7bf413a4b8a26d9fe706b1ff8e40081
sergioqfeg1 <sergioqfeg@gmail.com>
2024-04-04 17:34:32 +0200

    feat: method in repo for finding statistics by user

--- api/src/main/java/lab/en2b/quizapi/statistics/StatisticsRepository.java
+++ api/src/main/java/lab/en2b/quizapi/statistics/StatisticsRepository.java
@@ -1,6 +1,13 @@
 package lab.en2b.quizapi.statistics;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface StatisticsRepository extends JpaRepository<Statistics, Long> {
+
+    @Query(value = "SELECT * FROM Statistics WHERE user_id = ?1", nativeQuery = true)
+    Optional<Statistics> findByUserId(Long userId);
+
 }
