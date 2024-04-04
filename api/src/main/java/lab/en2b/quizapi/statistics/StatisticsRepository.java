6eddbb1458de12893481b01df513fb0594af3476
sergioqfeg1 <sergioqfeg@gmail.com>
2024-04-04 17:29:22 +0200

    feat: implementation of StatisticsRepository

---
+++ api/src/main/java/lab/en2b/quizapi/statistics/StatisticsRepository.java
@@ -0,0 +1,6 @@
+package lab.en2b.quizapi.statistics;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StatisticsRepository extends JpaRepository<Statistics, Long> {
+}
