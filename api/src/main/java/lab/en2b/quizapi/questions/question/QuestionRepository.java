a678fb2cdb93ec1425072388d0d3583e1466a599
sergioqfeg1 <sergioqfeg@gmail.com>
2024-04-06 14:59:29 +0200

    fix: problem with QuestionRepository sql

--- api/src/main/java/lab/en2b/quizapi/questions/question/QuestionRepository.java
+++ api/src/main/java/lab/en2b/quizapi/questions/question/QuestionRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface QuestionRepository extends JpaRepository<Question,Long> {
-    @Query(value = "SELECT q.* FROM questions q INNER JOIN answers a ON q.correct_answer_id=a.id WHERE a.language = ?1 ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
+    /*@Query(value = "SELECT q.* FROM questions q INNER JOIN answers a ON q.correct_answer_id=a.id WHERE a.language = ?1 ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
+    Question findRandomQuestion(String lang);*/
+
+    @Query(value = "SELECT q.* FROM questions q INNER JOIN answers a ON q.correct_answer_id=a.id WHERE q.language = ?1 ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
     Question findRandomQuestion(String lang);
 }
