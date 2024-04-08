41dc7f616c5110ac8bdc22971fe3f523e6d56c97
jjgancfer <jjgancfer@gmail.com>
2024-04-08 22:36:22 +0200

    feat: add a config to tests that work

---
+++ webapp/e2e/steps/about.steps.js
@@ -0,0 +1,60 @@
+const { defineFeature, loadFeature }=require('jest-cucumber');
+const puppeteer = require('puppeteer');
+const setDefaultOptions = require("expect-puppeteer").setDefaultOptions;
+const feature = loadFeature('./features/about_features/positive_logged_user_seeing_about_screen.feature');
+let page;
+let browser;
+
+defineFeature(feature, test => {
+
+    beforeAll(async () => {
+        browser = process.env.GITHUB_ACTIONS
+          ? await puppeteer.launch()
+          : await puppeteer.launch({ headless: false, slowMo: 100 });
+        page = await browser.newPage();
+        //Way of setting up the timeout
+        setDefaultOptions({ timeout: 10000 })
+    
+        await page
+          .goto("http://localhost:3000", {
+            waitUntil: "networkidle0",
+          })
+          .catch(() => {});
+      });
+
+      test("A logged user wants to see the about screen of the webpage", ({given,when,and,then}) => {
+        
+        let username;
+        let password;
+
+        given("A logged user in the main menu", async () => {
+            username = "test@email.com"
+            password = "password"
+            
+            await expect(page).toClick("button[data-testid='Login'");
+            await expect(page).toFill("#user", username);
+            await expect(page).toFill("#password", password);
+            await expect(page).toClick("button[data-testid='Login'");
+        });
+
+        when("The user presses the button for deploying the lateral menu", async () => {
+            await expect(page).toClick("#lateralMenuButton");
+        });
+
+        and("the user presses the button for seeing the about section (i)", async () => {
+            await expect(page).toClick("#aboutButton");
+        });
+
+        then("The user is presented to the about screen", async () => {
+            let header = await page.$eval("h2", (element) => {
+                return element.innerHTML
+              })
+            let value = header === "About" || header === "Sobre nosotros";
+            expect(value).toBeTruthy();
+        });
+      });
+
+      afterAll((done) => {
+        done();
+      });
+});
