be21382674887a6cf12d9403929e47249498c897
Gonzalo Alonso Fernandez <gony.soft@outlook.com>
2024-04-11 17:38:24 +0200

    feat: created the new custom game menu.

--- webapp/src/components/dashboard/CustomGameMenu.jsx
+++ webapp/src/components/dashboard/CustomGameMenu.jsx
@@ -1,21 +1,33 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { useNavigate } from "react-router-dom";
-import { useTranslation } from "react-i18next";
-import { Box, Drawer, DrawerOverlay, DrawerContent, DrawerCloseButton, DrawerHeader, DrawerBody, DrawerFooter, Select, Button, Text, Flex } from '@chakra-ui/react';
+import { Box, Drawer, DrawerOverlay, DrawerContent, DrawerCloseButton, DrawerHeader, DrawerBody, DrawerFooter, Button, Text, Flex, NumberInput, NumberInputField, NumberInputStepper, NumberIncrementStepper, NumberDecrementStepper } from '@chakra-ui/react';
 
 import AuthManager from "components/auth/AuthManager";
 
 const CustomGameMenu = ({ isOpen, onClose, changeLanguage }) => {
     const navigate = useNavigate();
-    const [selectedLanguage, setSelectedLanguage] = useState('');
-    const { t } = useTranslation();
+    const [selectedLanguage, setSelectedLanguage] = useState([]);
+    const [selectedGameType, setSelectedGameType] = useState([]);
+    const [rounds, setRounds] = useState(9);
+    const [time, setTime] = useState(20);
 
-    const handleChangeLanguage = (e) => {
-        const selectedValue = e.target.value;
-        setSelectedLanguage(selectedValue);
-        changeLanguage(selectedValue);
-    };   
+    const handleChangeLanguage = (language) => {
+        if (selectedLanguage.includes(language)) {
+            setSelectedLanguage(selectedLanguage.filter(item => item !== language));
+        } else {
+            setSelectedLanguage([...selectedLanguage, language]);
+        }
+        changeLanguage(language);
+    };
+
+    const handleGameTypeChange = (gameType) => {
+        if (selectedGameType.includes(gameType)) {
+            setSelectedGameType(selectedGameType.filter(item => item !== gameType));
+        } else {
+            setSelectedGameType([...selectedGameType, gameType]);
+        }
+    };
 
     const handleLogout = async () => {
         try {
@@ -31,29 +43,62 @@ const CustomGameMenu = ({ isOpen, onClose, changeLanguage }) => {
             <DrawerOverlay>
                 <DrawerContent>
                     <DrawerCloseButton />
-                    <Flex align="center">
-                        <DrawerHeader color={"forest_green.500"}>Custom game</DrawerHeader>
-                    </Flex>
+                    <DrawerHeader color={"forest_green.500"}>Custom game</DrawerHeader>
                     <DrawerBody>
                         <Box flexDirection="column">
-                            <Box margin={"10px"}>
-                                <Text color={"forest_green.500"}>{t("common.language")}</Text>
-                                <Select data-testid="language-select" onChange={handleChangeLanguage} value={selectedLanguage}>
-                                    <option value="es">{t("common.spanish")}</option>
-                                    <option value="en">{t("common.english")}</option>
-                                </Select>
+                            <Box>
+                                <Text fontWeight='extrabold' color={"forest_green.400"}>Game settings</Text>
+                                <Flex direction="column">
+                                    <Text color={"forest_green.400"}>Rounds</Text>
+                                    <NumberInput defaultValue={rounds} min={0} max={100} onChange={valueString => setRounds(parseInt(valueString))}>
+                                        <NumberInputField />
+                                        <NumberInputStepper>
+                                            <NumberIncrementStepper />
+                                            <NumberDecrementStepper />
+                                        </NumberInputStepper>
+                                    </NumberInput>
+                                    <Text color={"forest_green.400"}>Time</Text>
+                                    <NumberInput defaultValue={time} min={0} max={100} onChange={valueString => setTime(parseInt(valueString))}>
+                                        <NumberInputField />
+                                        <NumberInputStepper>
+                                            <NumberIncrementStepper />
+                                            <NumberDecrementStepper />
+                                        </NumberInputStepper>
+                                    </NumberInput>
+                                </Flex>
                             </Box>
-                            <Box margin={"10px"} flexDirection="row">
-                                <Box marginTop={"40px"}>
-                                    <Button data-testid="dashboard" type="submit" variant="link" colorScheme={"forest_green"} onClick={() => {navigate("/dashboard")}}>
-                                        <span style={{ marginLeft: '6px' }}>{t("common.dashboard")}</span>
-                                    </Button>
-                                </Box> 
+                            <Box marginTop="2em">
+                                <Text fontWeight='extrabold' color={"forest_green.400"}>Game type</Text>
+                                <Flex direction="column">
+                                    <Button className={"custom-button effect2"} variant={selectedGameType.includes('image') ? "solid" : "outline"} colorScheme="green" margin={"10px"} onClick={() => handleGameTypeChange('image')}>Image</Button>
+                                    <Button className={"custom-button effect2"} variant={selectedGameType.includes('text') ? "solid" : "outline"} colorScheme="green" margin={"10px"} onClick={() => handleGameTypeChange('text')}>Text</Button>
+                                </Flex>
                             </Box>
+                            {(selectedGameType.includes('image') || selectedGameType.includes('both')) && (
+                                <Box>
+                                    <Text color={"forest_green.500"}>Image categories</Text>
+                                    <Flex direction="column">
+                                        <Button className={"custom-button effect2"} variant={selectedLanguage.includes('football_stadium') ? "solid" : "outline"} colorScheme="green" margin={"10px"} onClick={() => handleChangeLanguage('football_stadium')}>Football Stadium</Button>
+                                        <Button className={"custom-button effect2"} variant={selectedLanguage.includes('drawings') ? "solid" : "outline"} colorScheme="green" margin={"10px"} onClick={() => handleChangeLanguage('drawings')}>Drawings</Button>
+                                    </Flex>
+                                </Box>
+                            )}
+                            {(selectedGameType.includes('text') || selectedGameType.includes('both')) && (
+                                <Box>
+                                    <Text color={"forest_green.500"}>Text categories</Text>
+                                    <Flex direction="column">
+                                        <Button className={"custom-button effect2"} variant={selectedLanguage.includes('ballon_dor') ? "solid" : "outline"} colorScheme="green" margin={"10px"} onClick={() => handleChangeLanguage('ballon_dor')}>Ballon D'or</Button>
+                                        <Button className={"custom-button effect2"} variant={selectedLanguage.includes('capitals') ? "solid" : "outline"} colorScheme="green" margin={"10px"} onClick={() => handleChangeLanguage('capitals')}>Capitals</Button>
+                                        <Button className={"custom-button effect2"} variant={selectedLanguage.includes('videogame_publishers') ? "solid" : "outline"} colorScheme="green" margin={"10px"} onClick={() => handleChangeLanguage('videogame_publishers')}>Videogame publishers</Button>
+                                    </Flex>
+                                </Box>
+                            )}
                         </Box>
                     </DrawerBody>
                     <DrawerFooter>
-                    <Button data-testid={"Save"} type="submit" colorScheme="pigment_green" margin={"10px"} className={"custom-button effect1"} onClick={handleLogout} width="100%">Save</Button>
+                        <Flex justify="flex-end" align="center" w="100%">
+                                <Button className={"custom-button effect1"} data-testid={"Save"} type="submit" colorScheme="forest_green" margin={"10px"} width="100%" onClick={handleLogout}>Save</Button>
+                        </Flex>
                     </DrawerFooter>
                 </DrawerContent>
             </DrawerOverlay>
