51c2a2145d28793bc393fa37903793d3704b68a0
sergioqfeg1 <sergioqfeg@gmail.com>
2024-04-11 20:32:43 +0200

    feat: getUserDetails controller

--- api/src/main/java/lab/en2b/quizapi/commons/user/UserController.java
+++ api/src/main/java/lab/en2b/quizapi/commons/user/UserController.java
@@ -1,6 +1,11 @@
 package lab.en2b.quizapi.commons.user;
 
+import lab.en2b.quizapi.commons.user.dtos.UserResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +16,9 @@ public class UserController {
 
     private final UserService userService;
 
-
+    @GetMapping("/details")
+    public ResponseEntity<UserResponseDto> getUserDetails(Authentication authentication) {
+        return ResponseEntity.ok(userService.getUserDetailsByAuthentication(authentication));
+    }
 
 }
