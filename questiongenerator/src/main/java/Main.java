4a5015cc3a9d1023800eeeb9e1182ff23b1200b5
Diego Villanueva <UO283615@uniovi.es>
2024-04-11 15:30:30 +0200

    Feat: Added the call to the questions

--- questiongenerator/src/main/java/Main.java
+++ questiongenerator/src/main/java/Main.java
@@ -2,30 +2,49 @@ import model.AnswerCategory;
 import repositories.GeneralRepositoryStorer;
 import templates.BallonDOrQuestion;
 import templates.CountryCapitalQuestion;
+import templates.PaintingQuestion;
+import templates.VideogamesPublisherQuestion;
 
 public class Main {
     public static void main(String[] args) {
 
+        // TEXT
         if(GeneralRepositoryStorer.doesntExist(AnswerCategory.CAPITAL_CITY)) {
             new CountryCapitalQuestion("en");
             new CountryCapitalQuestion("es");
         }
 
-        /*
-        if(GeneralRepositoryStorer.doesntExist(AnswerCategory.SONG.toString())) {
-            new SongQuestion("en");
-            new SongQuestion("es");
+        if (GeneralRepositoryStorer.doesntExist(AnswerCategory.BALLON_DOR)) {
+            new BallonDOrQuestion(""); // No need to specify language code as it is not used
         }
+
+        if (GeneralRepositoryStorer.doesntExist(AnswerCategory.GAMES_PUBLISHER)) {
+            new VideogamesPublisherQuestion("en");
+            new VideogamesPublisherQuestion("es");
+        }
+
+
+        /*
+        // IMAGES
 
         if(GeneralRepositoryStorer.doesntExist(AnswerCategory.STADIUM.toString())) {
             new StadiumQuestion("en");
             new StadiumQuestion("es");
         }
+
+        if (GeneralRepositoryStorer.doesntExist(AnswerCategory.PAINTING)) {
+            new PaintingQuestion("en");
+            new PaintingQuestion("es");
+        }
         */
 
-        if (GeneralRepositoryStorer.doesntExist(AnswerCategory.BALLON_DOR)) {
-            new BallonDOrQuestion(""); // No need to specify language code as it is not used
-        }
 
+        /*
+        // VIDEOS
+        if(GeneralRepositoryStorer.doesntExist(AnswerCategory.SONG.toString())) {
+            new SongQuestion("en");
+            new SongQuestion("es");
+        }
+        */
     }
 }
