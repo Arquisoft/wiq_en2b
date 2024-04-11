8fb1a444a28f62e50afaeca1b390afe8df27c076
Gonzalo Alonso Fernandez <gony.soft@outlook.com>
2024-04-11 10:04:16 +0200

    feat: integrated the user page with the dashboard.

--- webapp/src/components/Router.jsx
+++ webapp/src/components/Router.jsx
@@ -12,7 +12,6 @@ import Statistics from "pages/Statistics";
 import ProtectedRoute from "./utils/ProtectedRoute";
 import Logout from "pages/Logout";
 import About from "pages/About";
-import User from "pages/User";
 
 export default createRoutesFromElements(
   <Route path="/">
@@ -20,9 +19,9 @@ export default createRoutesFromElements(
     <Route path="/signup" element={<Signup />} />
     <Route path="/login" element={<Login />}/>
     <Route path="/about" element={<About />} />
-    <Route path="/user" element={<User />} />
+    <Route path="/dashboard" element={<Dashboard />}/>
     <Route path="/" element={<ProtectedRoute />}>
-      <Route path="/dashboard" element={<Dashboard />}/>
+
       <Route path="/dashboard/rules" element={<Rules />}/>
       <Route path="/dashboard/game" element={<Game />}/>
       <Route path="/dashboard/game/results" element={<Results />}/>
--- webapp/src/pages/Dashboard.jsx
+++ webapp/src/pages/Dashboard.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import { Heading, Button, Box, Stack } from "@chakra-ui/react";
+import { Heading, Button, Box, Stack, Tabs, TabList, Tab, TabPanels, TabPanel, Flex } from "@chakra-ui/react";
 import { Center } from "@chakra-ui/layout";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import { FaTachometerAlt } from 'react-icons/fa';
+import Avatar, { genConfig } from 'react-nice-avatar';
+import { SettingsIcon } from '@chakra-ui/icons';
 
 import AuthManager from "components/auth/AuthManager";
 import LateralMenu from '../components/LateralMenu';
@@ -28,15 +29,47 @@ export default function Dashboard() {
         i18n.changeLanguage(selectedLanguage);
     };
 
+    const config = genConfig("pepe@test.com") 
+    const user = {
+      username: "User1"
+    };
+
     return (
         <Center display="flex" flexDirection="column" w="100wh" h="100vh" justifyContent="center" alignItems="center" bgImage={'/background.svg'}>
           <MenuButton onClick={() => setIsMenuOpen(true)} />
           <LateralMenu isOpen={isMenuOpen} onClose={() => setIsMenuOpen(false)} changeLanguage={changeLanguage} isDashboard={true}/>
-          <FaTachometerAlt style={{ fontSize: '2.5rem', color: 'green' }} /> 
-          <Heading as="h2">{t("common.dashboard")}</Heading>
+          <Avatar style={{ width: '8rem', height: '8rem' }} {...config} /> 
+          <Heading as="h2">{t("common.welcome") + " " + user.username}</Heading>
     
           <Box minW={{ md: "400px" }} shadow="2xl">
             <Stack spacing={4} p="1rem" backgroundColor="whiteAlpha.900" boxShadow="md" rounded="1rem">
+              <Tabs isFitted variant='enclosed'>
+                <TabList mb='1em'>
+                  <Tab color="green.500">Game modes</Tab>
+                  <Tab color="green.500"><SettingsIcon mr={1}/> Settings</Tab>
+                </TabList>
+                <TabPanels>
+                  <TabPanel>
+                    <Flex justify="center" align="center" flexWrap="wrap">
+                      <Button colorScheme={"green"} variant="outline" textAlign="center" m="1em">
+                        Flags
+                      </Button>
+                      <Button colorScheme={"green"} variant="outline" textAlign="center" m="1em">
+                        Capitals
+                      </Button>
+                      <Button colorScheme={"green"} variant="outline" textAlign="center" m="1em">
+                        Football
+                      </Button>
+                      <Button colorScheme={"green"} variant="outline" textAlign="center" m="1em">
+                        Party
+                      </Button>
+                    </Flex>
+                  </TabPanel>
+                  <TabPanel>
+                    <p>user and game settings</p>
+                  </TabPanel>
+                </TabPanels>
+              </Tabs>
               <Button type="submit" data-testid={"Play"} variant={"solid"} colorScheme={"pigment_green"} margin={"5px"} size="lg" className={"custom-button effect1"} onClick={() => navigate("/dashboard/game")}>{t("common.play")}</Button>
               <Button type="submit" colorScheme="raw_umber" margin={"5px"} className={"custom-button effect1"} onClick={handleLogout}>{t("common.logout")}</Button>
             </Stack>
