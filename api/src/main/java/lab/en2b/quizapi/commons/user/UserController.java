53e1c6dc6964b9aa454685d0deaf14a3e3dd33b8
sergioqfeg1 <sergioqfeg@gmail.com>
2024-04-11 20:20:47 +0200

    feat: creation of UserController

---
+++ api/src/main/java/lab/en2b/quizapi/commons/user/UserController.java
@@ -0,0 +1,16 @@
+package lab.en2b.quizapi.commons.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+
+
+}
