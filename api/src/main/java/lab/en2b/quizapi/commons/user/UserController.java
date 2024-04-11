02e0bcf35e4003cd0b7e39d2c5e0735f1bc46d8d
sergioqfeg1 <sergioqfeg@gmail.com>
2024-04-11 20:34:25 +0200

    chore: add comments

--- api/src/main/java/lab/en2b/quizapi/commons/user/UserController.java
+++ api/src/main/java/lab/en2b/quizapi/commons/user/UserController.java
@@ -16,6 +16,13 @@ public class UserController {
 
     private final UserService userService;
 
+    /**
+     * Gets the user details for the given authentication
+     * Returns 200 if the user details are successfully retrieved
+     * Returns 403 if the user is not logged in
+     * @param authentication the authentication object
+     * @return the response dto for the user details
+     */
     @GetMapping("/details")
     public ResponseEntity<UserResponseDto> getUserDetails(Authentication authentication) {
         return ResponseEntity.ok(userService.getUserDetailsByAuthentication(authentication));
