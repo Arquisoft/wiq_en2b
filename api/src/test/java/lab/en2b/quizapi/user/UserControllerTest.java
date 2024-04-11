875f695768e166cd3924ac82ebefde578ccead89
sergioqfeg1 <sergioqfeg@gmail.com>
2024-04-11 20:40:23 +0200

    feat: getUserDetails controller test (200)

--- api/src/test/java/lab/en2b/quizapi/user/UserControllerTest.java
+++ api/src/test/java/lab/en2b/quizapi/user/UserControllerTest.java
@@ -4,12 +4,17 @@ import lab.en2b.quizapi.auth.config.SecurityConfig;
 import lab.en2b.quizapi.auth.jwt.JwtUtils;
 import lab.en2b.quizapi.commons.user.UserController;
 import lab.en2b.quizapi.commons.user.UserService;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
 @AutoConfigureMockMvc
@@ -24,5 +29,12 @@ public class UserControllerTest {
 
     @MockBean
     UserService userService;
+
+    @Test
+    void getUserShouldReturn200() throws Exception{
+        mockMvc.perform(get("/users/details")
+                        .with(user("test").roles("user")))
+                .andExpect(status().isOk());
+    }
 
 }
