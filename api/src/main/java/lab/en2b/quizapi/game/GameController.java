0c60bb3351f326656944943bc433a86b94544df9
Dario G. Mori <darionuberu@gmail.com>
2024-04-16 13:17:50 +0200

    feat: get current game when /play is called

--- api/src/main/java/lab/en2b/quizapi/game/GameController.java
+++ api/src/main/java/lab/en2b/quizapi/game/GameController.java
@@ -39,6 +39,21 @@ public class GameController {
             throw new IllegalArgumentException("Custom game mode requires a body");
         return ResponseEntity.ok(gameService.newGame(lang,gamemode,customGameDto,authentication));
     }
+    @Operation(summary = "Gets the current game", description = "Requests the API to get the current game")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
+            @ApiResponse(responseCode = "403", description = "You are not logged in", content = @io.swagger.v3.oas.annotations.media.Content),
+            @ApiResponse(responseCode = "403", description = "You are not logged in", content = @io.swagger.v3.oas.annotations.media.Content),
+    })
+    @Parameters({
+            @Parameter(name = "lang", description = "The language of the game", example = "en"),
+            @Parameter(name = "gamemode", description = "The gamemode of the game", example = "KIWI_QUEST")
+    })
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "The custom game dto, only required if the gamemode is CUSTOM")
+    @GetMapping("/play")
+    public ResponseEntity<GameResponseDto> getGame(Authentication authentication){
+        return ResponseEntity.ok(gameService.getGame(authentication));
+    }
 
     @Operation(summary = "Starts a new round", description = "Starts the round (asks a question and its possible answers to the API and start the timer) for a given authentication (a player)")
     @ApiResponses(value = {
