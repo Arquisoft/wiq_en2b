b7afbe85137e7bce211efba95b282098d53a4733
Dario G. Mori <darionuberu@gmail.com>
2024-04-16 11:45:50 +0200

    fix: get by id loads questions

--- api/src/main/java/lab/en2b/quizapi/questions/question/QuestionService.java
+++ api/src/main/java/lab/en2b/quizapi/questions/question/QuestionService.java
@@ -64,7 +64,9 @@ public class QuestionService {
     }
 
     public QuestionResponseDto getQuestionById(Long id) {
-        return questionResponseDtoMapper.apply(questionRepository.findById(id).orElseThrow());
+        Question q = questionRepository.findById(id).orElseThrow();
+        loadAnswers(q);
+        return questionResponseDtoMapper.apply(q);
     }
 
 
