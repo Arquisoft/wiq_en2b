37fe5edb19a961892baa4ea983487161267346f5
sergiorodriguezgarcia <113514397+sergiorodriguezgarcia@users.noreply.github.com>
2024-04-19 18:04:07 +0200

    feat: Counter answers centralized

--- webapp/src/pages/Results.jsx
+++ webapp/src/pages/Results.jsx
@@ -14,7 +14,7 @@ export default function Results() {
         <Center display="flex" flexDirection="column" w="100wh" h="100vh" justifyContent="center" alignItems="center" bgImage={'/background.svg'}>
             <Heading as="h2">{t("common.results")}</Heading>
             <Box bg="white" p={4} borderRadius="md" boxShadow="md" mt={4} mb={4} w="fit-content" shadow="2xl" rounded="1rem">
-                <Heading as="h3" color="green.400" fontSize="xl">{`Correct answers: ${correctAnswers}`}</Heading>
+                <Heading textAlign={"center"} as="h3" color="green.400" fontSize="xl">{`Correct answers: ${correctAnswers}`}</Heading>
                 <Flex direction="row" justifyContent="center" alignItems="center">
                     <Button data-testid={"GoBack"} type="submit" variant="solid" colorScheme="pigment_green" margin={"10px"} className={"custom-button effect1"} onClick={() => navigate("/dashboard")} w="100%">
                         {t("common.finish")}
